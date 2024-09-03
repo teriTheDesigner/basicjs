ad530394f3da2ebcb4dc0ba206ffc0b31af39e5f
teriTheDesigner <tere1109@stud.kea.dk>
2024-09-03 13:57:52 +0200

    events exercises

---
+++ 3sept/script.js
@@ -0,0 +1,21 @@
+const box = document.getElementById("box");
+
+box.addEventListener("click", function () {
+  if (box.style.backgroundColor === "red") {
+    box.style.backgroundColor = "aqua";
+  } else {
+    box.style.backgroundColor = "red";
+  }
+});
+
+const message = document.getElementById("message");
+
+document.getElementById("button").addEventListener("mouseover", function () {
+  message.classList.remove("hidden");
+  message.classList.add("visible");
+});
+
+document.getElementById("button").addEventListener("mouseout", function () {
+  message.classList.remove("visible");
+  message.classList.add("hidden");
+});
