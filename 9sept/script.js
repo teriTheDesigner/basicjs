0f88b145f55e0e8c1b503e20126237946a5c9198
teriTheDesigner <tere1109@stud.kea.dk>
2024-09-12 13:35:03 +0200

    number guessing game

--- 9sept/script.js
+++ 9sept/script.js
@@ -23,3 +23,54 @@ function addBook(book) {
 
   list.appendChild(newBook);
 }
+
+function findLargest(num1, num2, num3, num4) {
+  return Math.max(num1, num2, num3, num4);
+}
+
+const largestNumber = findLargest(12, 27, 102, 7);
+console.log(largestNumber);
+
+//Create a function that takes 3 numbers as 3 parameters. The first parameter is the number to check. The 2nd and 3rd parameter is a range (from and to). If the first parameter is within the range of the range it should return true and false if not.
+
+function checkRange(number, from, to) {
+  if (number >= from && number <= to) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(checkRange(5, 1, 7));
+console.log(checkRange(1, 5, 7));
+
+//Numbers guessing game
+
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+let numberOfGuesses = 0;
+let guess;
+
+document.getElementById("guessButton").addEventListener("click", checkValue);
+
+function checkValue() {
+  numberOfGuesses++;
+  let guess = Number(document.getElementById("guessInput").value);
+
+  if (guess === randomNumber) {
+    alert(
+      "Congratulations! The number was" +
+        " " +
+        randomNumber +
+        " " +
+        "it took you" +
+        " " +
+        numberOfGuesses +
+        " " +
+        "tries to guess it"
+    );
+  } else if (guess > randomNumber) {
+    alert("Too high! Try again.");
+  } else {
+    alert("Too low! Try again.");
+  }
+}
